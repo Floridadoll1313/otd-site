56b909efad25139320dc8ba4f1c9e325f78944e8
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-03-28 17:03:40 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This tells Vite exactly where to start
-  root: process.cwd(), 
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: './index.html', // Manually pointing to the entry point
-    },
   },
+  server: {
+    port: 3000,
+  }
 })
