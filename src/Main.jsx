0f098da1b4afff7c3dc57af11fc8924dcd8f41fd
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-03-28 12:21:59 -0400

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // This points to your App.js or App.jsx
-import './index.css';   // This pulls in your Neon Tailwind colors
+import App from './App'; // Vite will try to find App.js or App.jsx automatically here
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
