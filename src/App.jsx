b020ac5322c885a7785e5dde4ae04de4c18bf98f
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-04 12:22:53 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
-import GlobalHeader from "./components/GlobalHeader.js";
+import GlobalHeader from "./components/GlobalHeader.jsx";
 
 export default function App() {
   return (
