2f2c088afdd16c41e9c1a5121ddff4e28c4a1398
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-05 13:12:11 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
-import { createBrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Services from "./pages/Services";
@@ -19,4 +21,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-export default router;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
