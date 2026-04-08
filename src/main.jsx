bef5a8251bef6cdb4c318f51e7fa7be7a3a92635
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-08 12:15:47 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import App from "./App";
-
-// Pages
-import Home from "./pages/Home";
-import Services from "./pages/Services";
-import Realm from "./pages/Realm";
-import Contact from "./pages/Contact";
-import MemberPortal from "./MemberPortal";
-import MemberServices from "./MemberServices";
-import MemberPass from "./MemberPass";
-
-// Global Styles
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-
-        {/* App Shell */}
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="services" element={<Services />} />
-          <Route path="realm" element={<Realm />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="memberportal" element={<MemberPortal />} />
-          <Route
-            path="memberservices"
-            element={
-              <MemberPass>
-                <MemberServices />
-              </MemberPass>
-            }
-          />
-        </Route>
-
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
