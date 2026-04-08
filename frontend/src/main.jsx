4b55aa6a2f5c16ef62ee51cddf8ccc90b85703e1
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-04-08 17:27:57 -0400

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import router from "./router"; // Make sure you have router.js or router.jsx in src/
 
-// Import your pages
-import Home from "./pages/Home";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-
-// Add more pages here as needed
-// import About from "./pages/About";
-// import Membership from "./pages/Membership";
+import "./index.css"; // Only if you have Tailwind or global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="services" element={<Services />} />
-          <Route path="contact" element={<Contact />} />
-          {/* Add more routes here */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
